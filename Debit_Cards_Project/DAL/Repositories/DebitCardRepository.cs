485bf6bd9d3db64a2cf16662894b3f057c88251b
Дмитрий <semu-dima@yandex.ru>
2022-01-15 14:26:12 +0300

    added repositories and CRUD-operations

---
+++ Debit_Cards_Project/DAL/Repositories/DebitCardRepository.cs
@@ -0,0 +1,32 @@
+﻿using Debit_Cards_Project.DAL.Interfaces;
+
+namespace Debit_Cards_Project.DAL.Repositories
+{
+    public sealed class DebitCardRepository : IDebitCardRepository
+    {
+        public void Create(DebitCardRepository item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<DebitCardRepository> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DebitCardRepository ReadById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(DebitCardRepository item, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
