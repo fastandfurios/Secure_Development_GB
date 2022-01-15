63a7d5faf7d41d9e69ba4a0612c230036cd60a92
Дмитрий <semu-dima@yandex.ru>
2022-01-15 13:31:53 +0300

    added comments

--- Debit_Cards_Project/DAL/Models/DebitCard.cs
+++ Debit_Cards_Project/DAL/Models/DebitCard.cs
@@ -2,10 +2,17 @@
 {
     public sealed class DebitCard
     {
+        /// <summary> Название валюты </summary>
         public string CurrencyName { get; set; }
+        
+        /// <summary> Держатель карты </summary>
         public Holder Holder { get; set; }
         public int Id { get; set; }
+
+        /// <summary> Номер карты </summary>
         public long NumberCard { get; set; }
+
+        /// <summary> Срок действия карты </summary>
         public TimeSpan ValidityPeriod { get; set; }
     }
 }
