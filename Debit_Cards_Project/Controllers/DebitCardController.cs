5a46f4bdd3416cf5a3ddce852547acf686bb6efc
Дмитрий <semu-dima@yandex.ru>
2022-01-15 13:59:20 +0300

    added controller

---
+++ Debit_Cards_Project/Controllers/DebitCardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Debit_Cards_Project.Controllers
+{
+    [ApiController]
+    [Route("/debit_card")]
+    public class DebitCardController : ControllerBase
+    {
+        private readonly ILogger<DebitCardController> _logger;
+
+        public DebitCardController(ILogger<DebitCardController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
