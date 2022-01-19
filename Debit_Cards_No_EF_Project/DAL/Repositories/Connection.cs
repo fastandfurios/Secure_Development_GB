e7788e8f9f67c15680b46f5c28a888c98586c16e
Дмитрий <semu-dima@yandex.ru>
2022-01-19 21:19:07 +0300

    changed code

--- Debit_Cards_No_EF_Project/DAL/Repositories/Connection.cs
+++ Debit_Cards_No_EF_Project/DAL/Repositories/Connection.cs
@@ -5,7 +5,7 @@ namespace Debit_Cards_No_EF_Project.DAL.Repositories
 {
     public class Connection : IConnection
     {
-        private const string _connectionString = "Data Source=Debit_Cards.db;Version=3;Pooling=true;Max Pool Size=100;";
+        private const string _connectionString = "Data Source=Cards.db;Version=3;Pooling=true;Max Pool Size=100;";
 
         public SQLiteConnection GetOpenedConnection()
             => new(_connectionString);
