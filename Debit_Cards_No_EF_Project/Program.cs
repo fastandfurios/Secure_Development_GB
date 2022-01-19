c76813e7e2497cbf0668579449a87fcc5ca7317e
Дмитрий <semu-dima@yandex.ru>
2022-01-19 21:17:32 +0300

    registration and configuration of database services

--- Debit_Cards_No_EF_Project/Program.cs
+++ Debit_Cards_No_EF_Project/Program.cs
@@ -1,12 +1,24 @@
 using Debit_Cards_No_EF_Project.DAL.Interfaces;
 using Debit_Cards_No_EF_Project.DAL.Repositories;
+using FluentMigrator.Runner;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IDebitCardRepository, DebitCardRepository>();
 builder.Services.AddSingleton<IConnection, Connection>();
+
+var service = builder.Services.AddFluentMigratorCore()
+    .ConfigureRunner(configure => configure.AddSQLite()
+        .WithGlobalConnectionString("Data Source=Cards.db;Version=3;Pooling=true;Max Pool Size=100;")
+        .ScanIn(typeof(Program).Assembly).For.Migrations())
+    .AddLogging(configure => configure.AddFluentMigratorConsole())
+    .BuildServiceProvider(false);
+
+var runner = service.CreateScope().ServiceProvider.GetRequiredService<IMigrationRunner>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -24,5 +36,7 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+runner.MigrateUp();
 
 app.Run();
