26bbb3288f3b17dd4f346c5cea665a476767c40c
Дмитрий <semu-dima@yandex.ru>
2022-01-24 14:34:14 +0300

    added controller

---
+++ Debit_Cards_Project/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Debit_Cards_Project.DAL.Models.User;
+using Debit_Cards_Project.DAL.Models.User.Login;
+using Debit_Cards_Project.DAL.Models.User.Registration;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Debit_Cards_Project.Controllers
+{
+    [AllowAnonymous]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> LoginUserAsync(LoginQuery query)
+        {
+            return await _mediator.Send(query);
+        }
+
+        [HttpPost("registration")]
+        public async Task<ActionResult<User>> RegistrationUserAsync(RegistrationCommand command)
+        {
+            return await _mediator.Send(command);
+        }
+    }
+}
