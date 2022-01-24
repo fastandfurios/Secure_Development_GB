52335c6b341a6fa1407a0c998dee5f67050850b3
Дмитрий <semu-dima@yandex.ru>
2022-01-24 14:27:35 +0300

    added context

---
+++ Debit_Cards_Project/DAL/Context/UsersDb.cs
@@ -0,0 +1,13 @@
+﻿using Debit_Cards_Project.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Debit_Cards_Project.DAL.Context
+{
+    public class UsersDb : IdentityDbContext<AppUser>
+    {
+        public UsersDb(DbContextOptions<UsersDb> options) : base(options)
+        {
+        }
+    }
+}
