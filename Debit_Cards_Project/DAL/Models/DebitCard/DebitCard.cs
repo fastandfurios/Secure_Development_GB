8cbc78c29536ac7745b6d802f98df01cd5946774
Дмитрий <semu-dima@yandex.ru>
2022-01-31 22:10:46 +0300

    added folder

---
+++ Debit_Cards_Project/DAL/Models/DebitCard/DebitCard.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Debit_Cards_Project.DAL.Models.DebitCard
+{
+    public sealed class DebitCard
+    {
+        /// <summary> Название валюты </summary>
+        public string CurrencyName { get; set; }
+        
+        /// <summary> Держатель карты </summary>
+        public Holder Holder { get; set; }
+
+        [JsonIgnore]
+        public int Id { get; set; }
+
+        /// <summary> Номер карты </summary>
+        public long NumberCard { get; set; }
+
+        /// <summary> Месяц действия </summary>
+        public int Month { get; set; }
+
+        /// <summary> Год действия  </summary>
+        public int Year { get; set; }
+    }
+}
