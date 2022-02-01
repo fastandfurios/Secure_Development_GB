771907c567e220c1f0bc8f69a612983ae436a79e
Дмитрий <semu-dima@yandex.ru>
2022-02-01 23:57:15 +0300

    changed the context code

--- Debit_Cards_Project/DAL/Context/DebitCardsDB.cs
+++ Debit_Cards_Project/DAL/Context/DebitCardsDB.cs
@@ -6,6 +6,7 @@ namespace Debit_Cards_Project.DAL.Context
     public sealed class DebitCardsDb : DbContext
     {
         public DbSet<DebitCard> Cards { get; set; }
+        public DbSet<Holder> Holders { get; set; }
 
         public DebitCardsDb(DbContextOptions<DebitCardsDb> options) : base(options) 
             => Database.EnsureCreated();
