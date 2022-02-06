29c5399eed2245168f2e46e46e7ddf2241acffa7
Дмитрий <semu-dima@yandex.ru>
2022-02-07 00:54:51 +0300

    added model

---
+++ Debit_Cards_Project/DAL/Models/CashBack/CashBack.cs
@@ -0,0 +1,10 @@
+﻿namespace Debit_Cards_Project.DAL.Models.CashBack
+{
+    public sealed class CashBack
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public double Percent { get; set; }
+    }
+}
