fceaf3249bc8443912f29b00842436e82528a8f9
Дмитрий <semu-dima@yandex.ru>
2022-02-07 01:03:23 +0300

    added chain of responsibilities interfaces

---
+++ Debit_Cards_Project/Infrastructure/ChainOfResponsibility/Interfaces/IChain.cs
@@ -0,0 +1,7 @@
+﻿namespace Debit_Cards_Project.Infrastructure.ChainOfResponsibility.Interfaces
+{
+    public interface IChain
+    {
+        object GetCashBack(string request);
+    }
+}
---
+++ Debit_Cards_Project/Infrastructure/ChainOfResponsibility/Interfaces/IHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace Debit_Cards_Project.Infrastructure.ChainOfResponsibility.Interfaces
+{
+    public interface IHandler
+    {
+        IHandler SetNext(IHandler handler);
+        object Handle(object request);
+    }
+}
