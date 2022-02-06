43670dbf3ed535f0d95fd6846078f6104ebbe316
Дмитрий <semu-dima@yandex.ru>
2022-02-07 01:04:19 +0300

    implementation of chain of responsibility interfaces

---
+++ Debit_Cards_Project/Infrastructure/ChainOfResponsibility/Implementations/AbstractHandler.cs
@@ -0,0 +1,20 @@
+﻿using Debit_Cards_Project.Infrastructure.ChainOfResponsibility.Interfaces;
+
+namespace Debit_Cards_Project.Infrastructure.ChainOfResponsibility.Implementations
+{
+    public class AbstractHandler : IHandler
+    {
+        private IHandler _nextHandler;
+
+        public IHandler SetNext(IHandler handler)
+        {
+            _nextHandler = handler;
+            return handler;
+        }
+
+        public virtual object Handle(object request)
+        {
+            return _nextHandler is not null ? _nextHandler.Handle(request) : default;
+        }
+    }
+}
