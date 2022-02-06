7a74f60ea54f483f1095b15d8887b76234b9fcc4
Дмитрий <semu-dima@yandex.ru>
2022-02-07 00:59:49 +0300

    added an abstract factory interface

---
+++ Debit_Cards_Project/Infrastructure/AbstractFactory/Interfaces/ICashBackOption.cs
@@ -0,0 +1,9 @@
+﻿namespace Debit_Cards_Project.Infrastructure.AbstractFactory.Interfaces
+{
+    public interface ICashBackOption
+    {
+        string Category { get; set; }
+        string Description { get; set; }
+        double Percent { get; set; }
+    }
+}
