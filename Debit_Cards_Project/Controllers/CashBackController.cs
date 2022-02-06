8b2fe3a92a957c2c9bd33aa070ca9e260ab09dab
Дмитрий <semu-dima@yandex.ru>
2022-02-07 01:04:51 +0300

    added another controller

---
+++ Debit_Cards_Project/Controllers/CashBackController.cs
@@ -0,0 +1,48 @@
+﻿using Debit_Cards_Project.Infrastructure.ChainOfResponsibility.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Debit_Cards_Project.Controllers
+{
+    [ApiController]
+    [Route("/cash_back")]
+    [Authorize]
+    public class CashBackController : ControllerBase
+    {
+        private readonly IChain _chain;
+        private readonly ILogger<CashBackController> _logger;
+
+        public CashBackController(IChain chain, ILogger<CashBackController> logger)
+        {
+            _chain = chain;
+            _logger = logger;
+        }
+
+        [HttpPost("add_cash_back/{category}")]
+        public IActionResult Create([FromRoute] string category)
+        {
+
+            var cashback = _chain.GetCashBack(category);
+
+            return Ok();
+        }
+
+        [HttpGet("cash_back/{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("all_cash_back")]
+        public IActionResult GetAllCashBackOptions()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("remove_cash_back/{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            return Ok();
+        }
+    }
+}
