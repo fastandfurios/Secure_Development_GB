f9c9424820efcefc845cf0ca68c21ea6e99dda97
Дмитрий <semu-dima@yandex.ru>
2022-02-07 14:11:00 +0300

    mapping settings

--- Debit_Cards_Project/Mapping/MappingProfile.cs
+++ Debit_Cards_Project/Mapping/MappingProfile.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
+using Debit_Cards_Project.DAL.Models.CashBack;
 using Debit_Cards_Project.DAL.Models.DebitCard;
+using Debit_Cards_Project.Domain.Products;
+using Debit_Cards_Project.Domain.Services;
 using Debit_Cards_Project.DTO;
 
 namespace Debit_Cards_Project.Mapping
@@ -10,6 +13,8 @@ namespace Debit_Cards_Project.Mapping
         {
             CreateMap<DebitCard, DebitCardDto>();
             CreateMap<Holder, HolderDto>();
+            CreateMap<CashBack, CashBackDto>()
+                .ForMember(dest => dest.Percent, oct => oct.MapFrom(src => $"{src.Percent:F1} %"));
         }
     }
 }
