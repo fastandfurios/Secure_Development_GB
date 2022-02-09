998505236940254cfae0ecaaed2826ff7bf5902f
Дмитрий <semu-dima@yandex.ru>
2022-02-09 23:28:50 +0300

    added the nlog service

--- Biblio/Program.cs
+++ Biblio/Program.cs
@@ -4,39 +4,67 @@ using Biblio.DAL.Models.Book;
 using Biblio.DAL.Repositories;
 using Biblio.Mapping;
 using FluentValidation;
-
+using NLog;
+using NLog.Web;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 #endregion
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.Setup()
+    .LoadConfigurationFromAppSettings()
+    .GetCurrentClassLogger();
 
-#region Add services to the container.
-builder.Services.AddControllers();
+logger.Debug("init main");
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddScoped<IBookRepository, BookRepository>();
-
-builder.Services.Configure<BookStoreDatabaseSettings>(builder.Configuration.GetSection("BooksDb"));
-
-builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MappingProfile>());
-
-builder.Services.AddScoped<IValidator<Book>, BookValidation>();
-#endregion
-
-var app = builder.Build();
-
-#region Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    #region Add services to the container.
+
+    builder.Services.AddControllers();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddScoped<IBookRepository, BookRepository>();
+
+    builder.Services.Configure<BookStoreDatabaseSettings>(builder.Configuration.GetSection("BooksDb"));
+
+    builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MappingProfile>());
+
+    builder.Services.AddScoped<IValidator<Book>, BookValidation>();
+
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(LogLevel.Trace);
+    builder.Host.UseNLog();
+
+    #endregion
+
+    var app = builder.Build();
+
+    #region Configure the HTTP request pipeline.
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
+    #endregion
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-#endregion
+catch (Exception e)
+{
+    logger.Error(e, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    LogManager.Shutdown();
+}
