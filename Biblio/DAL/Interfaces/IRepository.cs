5e76367bcdcf7ac7d7eda23e47c09ae08edda027
Дмитрий <semu-dima@yandex.ru>
2022-02-09 20:51:39 +0300

    added repository interfaces

---
+++ Biblio/DAL/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Biblio.DAL.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task AddAsync(T entity, CancellationToken token=default);
+        Task<IReadOnlyList<T>> GetAllAsync(CancellationToken token=default);
+        Task<T> GetEntityAsync(string id, CancellationToken token=default);
+        Task UpdateAsync(T entity, string id, CancellationToken token=default);
+        Task DeleteAsync(string id, CancellationToken token=default);
+    }
+}
