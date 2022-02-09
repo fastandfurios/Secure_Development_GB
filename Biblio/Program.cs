b916cfd9aef0b7c1d76aa8ccc68e777efc670842
Дмитрий <semu-dima@yandex.ru>
2022-02-09 20:53:31 +0300

    added services

--- Biblio/Program.cs
+++ Biblio/Program.cs
@@ -1,15 +1,26 @@
+#region References
+using Biblio.DAL.Interfaces;
+using Biblio.DAL.Models.Book;
+using Biblio.DAL.Repositories;
+#endregion
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+#region Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+
+builder.Services.Configure<BookStoreDatabaseSettings>(builder.Configuration.GetSection("BooksDb"));
+#endregion
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+#region Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,3 +32,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+#endregion
