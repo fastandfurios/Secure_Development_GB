2596c9bb7a300b5b5ed857a7f18aeb0c18926869
Дмитрий <semu-dima@yandex.ru>
2022-02-09 21:11:15 +0300

    added profil

---
+++ Biblio/Mapping/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Biblio.DAL.Models.Book;
+using Biblio.DTOs;
+
+namespace Biblio.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Book, BookDto>();
+        }
+    }
+}
