e2743dc571185c4de5ec19aa0c85ac22f8bd7eda
Дмитрий <semu-dima@yandex.ru>
2022-02-18 21:36:50 +0300

    added search

--- Books/Controllers/HomeController.cs
+++ Books/Controllers/HomeController.cs
@@ -1,21 +1,58 @@
 ﻿using Books.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Nest;
 
 namespace Books.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ElasticClient _client;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ElasticClient client)
         {
             _logger = logger;
+            _client = client;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string query)
         {
-            return View();
+            ISearchResponse<Book> results;
+
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                results = _client.Search<Book>(s => s
+                    .Query(q => q
+                        .Match(t => t
+                            .Field(f => f.Title)
+                            .Field(f => f.ShortDescription)
+                            .Query(query)
+                        )
+                    )
+                );
+            }
+            else
+            {
+                results = _client.Search<Book>(s => s
+                    .Query(q => q
+                        .MatchAll()
+                    )
+                    .Aggregations(a => a
+                        .Range("pageCounts", r => r
+                            .Field(f => f.PageCount)
+                            .Ranges(r => r.From(0),
+                                r => r.From(200).To(400),
+                                r => r.From(400).To(600),
+                                r => r.From(600))
+                        )
+                        .Terms("categories", t => t
+                            .Field("categories.keyword"))
+                    )
+                );
+            }
+
+            return View(results);
         }
 
         public IActionResult Privacy()
