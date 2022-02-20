41a4348cb786392836b994462c602b0925bb33b6
Дмитрий <semu-dima@yandex.ru>
2022-02-20 17:15:26 +0300

    registered services

--- ServiceA/Program.cs
+++ ServiceA/Program.cs
@@ -1,15 +1,29 @@
+#region references
+using Consul;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using ServiceA.Services;
+
+#endregion
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IConsulClient, ConsulClient>(serviceProvider => new(configOverride =>
+{
+    configOverride.Address = new(builder.Configuration.GetConnectionString("Consul"));
+}));
+builder.Services.AddSingleton<IHostedService, LaunchService>();
+#endregion
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+#region Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,3 +35,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+#endregion
