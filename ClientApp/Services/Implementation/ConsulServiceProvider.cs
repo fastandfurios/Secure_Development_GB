f04dede1235b10e0964417c58ba92f7caac56be3
Дмитрий <semu-dima@yandex.ru>
2022-02-20 23:23:34 +0300

    added an interface implementation

---
+++ ClientApp/Services/Implementation/ConsulServiceProvider.cs
@@ -0,0 +1,27 @@
+﻿using ClientApp.Services.Interfaces;
+using Consul;
+
+namespace ClientApp.Services.Implementation
+{
+    public class ConsulServiceProvider : IServiceDiscoveryProvider
+    {
+        public async Task<List<string>> GetServicesAsync(CancellationToken token = default)
+        {
+            var consulClient = new ConsulClient(configOverride =>
+            {
+                configOverride.Address = new("http://localhost:8500");
+            });
+
+            var queryResult = await consulClient.Health
+                .Service(service: "ServiceA", tag: "api", passingOnly: true, ct: token)
+                .ConfigureAwait(true);
+            var result = new List<string>();
+            foreach (var service in queryResult.Response)
+            {
+                result.Add($"{service.Service.Address}:{service.Service.Port}");
+            }
+
+            return result;
+        }
+    }
+}
