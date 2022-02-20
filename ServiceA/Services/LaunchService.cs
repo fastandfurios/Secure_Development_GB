29a88d31f6522a8b540efea40857c8fa1d4bdd90
Дмитрий <semu-dima@yandex.ru>
2022-02-20 17:15:49 +0300

    added service

---
+++ ServiceA/Services/LaunchService.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using Consul;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+
+namespace ServiceA.Services
+{
+    public class LaunchService : IHostedService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IConsulClient _consulClient;
+        private CancellationTokenSource _cts;
+        private string _serviceId;
+
+        public LaunchService(IConsulClient consulClient, IConfiguration configuration)
+        {
+            _consulClient = consulClient;
+            _configuration = configuration;
+        }
+
+        public async Task StartAsync(CancellationToken token = default)
+        {
+            _cts = CancellationTokenSource.CreateLinkedTokenSource(token);
+            var uri = new Uri(_configuration.GetConnectionString("ServiceA"));
+            _serviceId = $"Service-v1-{Dns.GetHostName()}-{uri.Authority}";
+            var registration = new AgentServiceRegistration
+            {
+                ID = _serviceId,
+                Name = "ServiceA",
+                Address = uri.Host,
+                Port = uri.Port,
+                Tags = new[] { "api" }
+            };
+
+            await _consulClient.Agent.ServiceDeregister(registration.ID, _cts.Token);
+            await _consulClient.Agent.ServiceRegister(registration, _cts.Token);
+        }
+
+        public async Task StopAsync(CancellationToken token = default)
+        {
+            _cts.Cancel();
+            await _consulClient.Agent.ServiceDeregister(_serviceId, token);
+        }
+    }
+}
