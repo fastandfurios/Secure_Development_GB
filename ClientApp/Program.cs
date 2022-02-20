9530c5262df0a0013a14be8994ca12b6252e033a
Дмитрий <semu-dima@yandex.ru>
2022-02-20 23:24:10 +0300

    calling the provider

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClientApp.Services.Implementation;
+using ClientApp.Services.Interfaces;
+
+await Task.Delay(40000).ConfigureAwait(true);
+IServiceDiscoveryProvider consul = new ConsulServiceProvider();
+var r = await consul.GetServicesAsync(CancellationToken.None);
+IServiceDiscoveryProvider pollingConsul = new PollingConsulServiceProvider();
+var result = await pollingConsul.GetServicesAsync(CancellationToken.None);
+foreach (var entry in result)
+{
+    Console.WriteLine(entry);
+}
+Console.ReadLine();
